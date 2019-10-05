4460bc69e98c84ddd538cd3cd5c4a58226ee7af9
unknown <chris.a.desilva@gmail.com>
2019-10-05 16:10:18 -0500

    Add SCSS, create mobile navbar with hamburger menu and tablet+ menu with visible links

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,59 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
+import { Collapse } from 'react-collapse'
 import logo from "../images/strive_logo.png"
 
-const Header = ({ siteTitle }) => (
-  <header id="navbar">
-    <div id="navbar__logo">
-      <img src={logo} alt="CrossFit Strive logo" />
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+
+  const [isMenuOpen, toggleMenu] = useState(false)
+
+  return (
+    <>
+    <header id="navbar">
+      <div id="navbar-closed">
+        <div id="navbar__logo">
+          <Link to="/"><img src={logo} alt="CrossFit Strive logo" /></Link>
+        </div>
+
+        <div onClick={() => toggleMenu(!isMenuOpen)} id="navbar__hamburger"  className={isMenuOpen ? "animate" : null}>
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+      </div>
+
+      <div id="navbar-open">
+        <Collapse isOpened={isMenuOpen}>
+          <div id="navbar__links">
+            <Link to="/classes">Classes</Link>
+            <Link to="/about">About</Link>
+            <Link to="/reviews">Reviews</Link>
+            <a className="btn btn-primary" href="https://crossfitstrivebastrop.wodify.com/OnlineSalesPortal/ViewSchedule.aspx?LocationId=4207&IsMobile=False&OnlineMembershipId=48149" target="_blank" rel="noreferrer noopener">Sign Me Up</a>
+          </div>
+        </Collapse>
+      </div>
+
+    </header>
+
+    <header id="navbar--md">
+        <div id="navbar--md__logo">
+          <Link to="/"><img src={logo} alt="CrossFit Strive logo" /></Link>
+        </div>
+
+      <div id="navbar--md-open">
+          <div id="navbar--md__links">
+            <Link className="link" to="/classes">Classes</Link>
+            <Link className="link" to="/about">About</Link>
+            <Link className="link" to="/reviews">Reviews</Link>
+            <a className="btn btn-primary" href="https://crossfitstrivebastrop.wodify.com/OnlineSalesPortal/ViewSchedule.aspx?LocationId=4207&IsMobile=False&OnlineMembershipId=48149" target="_blank" rel="noreferrer noopener">Sign Me Up</a>
+          </div>
+      </div>
+
+    </header>
+    </>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,62 @@
 import React from "react"
-import TextLoop from "react-text-loop"
 import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="container">
-      <div id="leadContent">
-      <h1>
-        You’ll sweat. you’ll laugh. you’ll get stronger.  We promise. 
-      </h1>
-        <button className="btn btn-primary">Sign Me Up!</button>
-      </div>
-      <div className="joinUs">
-        <h2>
-        Want to {" "}
-          <TextLoop
-            springConfig={{stiffness: 50, damping: 20}}
-          >
-            <span>lose weight?</span>
-            <span>feel healthier?</span>
-            <span>challenge yourself?</span>
-            <span>have fun?</span>
-          </TextLoop>
-        </h2>
-        <p>
-          Welcome to CrossFit Strive! Our mission is to empower people to be happier, healthier, and more successful in life. We’re a family first, CrossFit gym second. Whatever your health and fitness goals are, our experienced coaches and supportive members will help you get there.   We promise to teach you, hold you accountable, and cheer you on every step of the way. 
-        </p>
+const IndexPage = () => {
 
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "pushups_bumpers.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="container">
+        
+        <div id="leadContent">
+          <h1>
+            You’ll sweat. you’ll laugh. you’ll get stronger.  We promise. 
+          </h1>
+          <a className="btn btn-primary" href="https://crossfitstrivebastrop.wodify.com/OnlineSalesPortal/ViewSchedule.aspx?LocationId=4207&IsMobile=False&OnlineMembershipId=48149" target="_blank" rel="noreferrer noopener">Sign Me Up</a>
+        </div>
+
+        <div className="schedule">
+          <h2>Classes</h2>
+          <p>CrossFit</p>
+          <p>Pump 30/Burn 30</p>
+          <p>Barbell Club</p>
+        </div>
+
+        <div className="joinUs">
+          <h2>
+          Want to lose weight?
+              <span> feel healthier?</span>
+              <span> challenge yourself?</span>
+              <span> have fun?</span>
+          </h2>
+          <div style={{width: `100%`, marginBottom: `1.45rem`, padding: `0 1rem`}}><Img fluid={data.file.childImageSharp.fluid} /></div>
+          <p>
+            Welcome to CrossFit Strive! Our mission is to empower people to be happier, healthier, and more successful in life. We’re a family first, CrossFit gym second. Whatever your health and fitness goals are, our experienced coaches and supportive members will help you get there.   We promise to teach you, hold you accountable, and cheer you on every step of the way. 
+          </p>
+        </div>
+
+        <div className="contact">
+        </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default IndexPage
