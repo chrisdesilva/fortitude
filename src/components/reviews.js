e678482ea48b06361efb0c560097aae592d8cae1
unknown <chris.a.desilva@gmail.com>
2019-11-03 09:31:27 -0600

    Update grid layout for fb reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Img from 'gatsby-image'
 
-const Reviews = props => (
-  <div className="reviews">
-  <h2 data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out">Reviews</h2>
-  <div data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out" className="reviews__bestOf">
-    <div><Img fluid={props.best2018} alt="Best of Bastrop 2018 award"/></div>
-    <div><Img fluid={props.best2019} alt="Best of Bastrop 2019 award"/></div>
-  </div>
-  <div data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out" className="reviews__cards">
-    <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ffish1200%2Fposts%2F10217125698044336&width=500" width="500" height="256" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>  
-    <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjoshua.bingaman%2Fposts%2F10217120466104389&width=500" width="500" height="161" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-    <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fchelsea.l.forrest.3%2Fposts%2F10156382843612787&width=500" width="500" height="199" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-    <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ftimothy.sharlow.3%2Fposts%2F1186811554794133%3A0&width=500" width="500" height="354" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>   
-    <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmarroquintorres27%2Fposts%2F10157477835996763&width=500" width="500" height="180" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>  
-    <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fveeguns1%2Fposts%2F10156052718412838%3A0&width=500" width="500" height="392" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>  
+const Reviews = props => {
+
+  useEffect(() => {
+    fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/crossfit-strive-bastrop-bastrop-2/reviews')
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+      })
+  }, [])
+
+  return (
+    <div className="reviews">
+    <h2 data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out">Reviews</h2>
+    <div data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out" className="reviews__bestOf">
+      <div><Img fluid={props.best2018} alt="Best of Bastrop 2018 award"/></div>
+      <div><Img fluid={props.best2019} alt="Best of Bastrop 2019 award"/></div>
     </div>
-  </div>
-)
+    <div data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out" className="reviews__cards">
+      <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ffish1200%2Fposts%2F10217125698044336&width=500" width="500" height="256" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>  
+      <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fjoshua.bingaman%2Fposts%2F10217120466104389&width=500" width="500" height="161" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+      <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ftimothy.sharlow.3%2Fposts%2F1186811554794133%3A0&width=500" width="500" height="354" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>   
+      <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fchelsea.l.forrest.3%2Fposts%2F10156382843612787&width=500" width="500" height="199" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+      <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmarroquintorres27%2Fposts%2F10157477835996763&width=500" width="500" height="180" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>  
+      <iframe className="reviews__iframe" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fveeguns1%2Fposts%2F10156052718412838%3A0&width=500" width="500" height="392" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>  
+    </div>
+    </div>
+  )
+}
 
 export default Reviews
