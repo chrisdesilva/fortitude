bcd0a276d54e5a2086c45baf18d246767b0193be
unknown <chris.a.desilva@gmail.com>
2019-11-06 18:43:06 -0600

    Update pricing

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -5,7 +5,33 @@ const Pricing = () => {
   return (
     <div data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out" className="pricing">
       <h2>Pricing</h2>
-      <p>Drop-In: $20/class</p>
+      <table>
+        <tr>
+          <th colSpan="3">12 Month Contract</th>
+        </tr>
+        <tr className="row">
+          <td>2x/week: $90/mo</td>
+          <td>3x/week: $103/mo</td>
+          <td>Unlimited: $131/mo</td>
+        </tr>
+        <tr>
+          <th colSpan="3">6 Month Contract</th>
+        </tr>
+        <tr className="row">
+          <td>2x/week: $104/mo</td>
+          <td>3x/week: $118/mo</td>
+          <td>Unlimited: $151/mo</td>
+        </tr>
+        <tr>
+          <th colSpan="3">3 Month Contract</th>
+        </tr>
+        <tr className="row">
+          <td>2x/week: $119/mo</td>
+          <td>3x/week: $136/mo</td>
+          <td>Unlimited: $173/mo</td>
+        </tr>
+      </table>
+{/*       <p>Drop-In: $20/class</p>
       <p>2x/week: $100/mo</p>
       <p>3x/week: $120/mo</p>
       <p>Unlimited: $130/mo</p>
@@ -13,7 +39,7 @@ const Pricing = () => {
       <p style={{margin: 0}}><strong>Family Discount</strong></p>
       <p>10% off each membership</p>
       <p style={{margin: 0}}><strong>Hero Discount</strong></p>
-      <p>20% off for active military, first responders, vetarans, and teachers</p>
+      <p>20% off for active military, first responders, vetarans, and teachers</p> */}
     </div>
   )
 }
