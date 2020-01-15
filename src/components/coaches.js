882349153732a3809adefb2835e68476e1dcc8a1
unknown <chris.a.desilva@gmail.com>
2020-01-14 19:29:05 -0600

    Update coach bios and headshots

--- src/components/coaches.js
+++ src/components/coaches.js
@@ -1,52 +1,169 @@
-import React from 'react'
-import Img from 'gatsby-image'
+import React from "react"
+import Img from "gatsby-image"
 
 const Coaches = props => {
-
   return (
     <div className="coaches">
-    <h2 data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out">Coaches</h2>
-      <div data-sal="fade" data-sal-duration="500" data-sal-easing="ease-in-out" className="coaches__row">
+      <h2
+        data-sal="fade"
+        data-sal-duration="1000"
+        data-sal-easing="ease-in-out"
+      >
+        Coaches
+      </h2>
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
         <div className="coaches__photo">
           <Img fluid={props.athan} alt="Coach Athan" />
         </div>
         <div className="coaches__bio">
           <h3>Coach Athan</h3>
-          <p>Athan Schindler is a former U.S. Army Airborne Ranger. In addition to owning and coaching at two CrossFit gyms, he currently serves as a Master Fitness Trainer for the U.S. Army.  But don’t be intimidated- Athan has a big smile and a big heart. His mission in life is to help others achieve optimal health, deep human connection, and self-actualization.
+          <p>
+            Athan Schindler is a former U.S. Army Airborne Ranger. In addition
+            to owning and coaching at two CrossFit gyms, he currently serves as
+            a Master Fitness Trainer for the U.S. Army. But don’t be
+            intimidated- Athan has a big smile and a big heart. His mission in
+            life is to help others achieve optimal health, deep human
+            connection, and self-actualization.
           </p>
+          <p></p>
         </div>
       </div>
-      <div data-sal="fade" data-sal-duration="500" data-sal-easing="ease-in-out" className="coaches__row">
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
         <div className="coaches__photo">
           <Img fluid={props.kady} alt="Coach Kady" />
         </div>
         <div className="coaches__bio">
           <h3>Coach Kady</h3>
-          <p>Kady Skinner was voted the Best Fitness Instructor in Bastrop for 2019. She has been coaching and competing in CrossFit for over seven years. Kady is knowledgeable, encouraging, and loud (in a good way).  She’s a Fit Mom who believes that strength is beauty!
+          <p>
+            Kady Skinner was voted the Best Fitness Instructor in Bastrop for
+            2019. She has been coaching and competing in CrossFit for over seven
+            years. Kady is knowledgeable, encouraging, and loud (in a good way).
+            She’s a Fit Mom who believes that strength is beauty!
           </p>
-        </div>        
-
+          <p className="coaches__bio--cert">
+            Certifications: Bachelor's in Applied Exercise Physiology from Texas
+            A&M. NASM PT certification, Yoga 1 certification, CF L1, CF L2, CF
+            kids, CF Endurance, USA weightlifting sports performance coach
+          </p>
+        </div>
       </div>
-      <div data-sal="fade" data-sal-duration="500" data-sal-easing="ease-in-out" className="coaches__row">
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
         <div className="coaches__photo">
           <Img fluid={props.cody} alt="Coach Cody" />
         </div>
         <div className="coaches__bio">
           <h3>Coach Cody</h3>
-          <p>Cody Skinner is the founder of Fortitude Barbell Club and Operations Manager at CrossFit Strive.  With over seven years of experience in CrossFit and six years in competitive weightlifting, Cody is sometimes referred to as “the Yoda of the Barbell” due to his ability to break down and teach complex movements.
+          <p>
+            Cody Skinner is the founder of Fortitude Barbell Club and Operations
+            Manager at CrossFit Strive. With over seven years of experience in
+            CrossFit and six years in competitive weightlifting, Cody is
+            sometimes referred to as “the Yoda of the Barbell” due to his
+            ability to break down and teach complex movements.
+          </p>
+          <p>
+            Certifications: USAW Advanced Sport Performance Coach, CF L1, NASM
+            CPT (expired)
           </p>
         </div>
       </div>
-      <div data-sal="fade" data-sal-duration="500" data-sal-easing="ease-in-out" className="coaches__row">
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
         <div className="coaches__photo">
           <Img fluid={props.lexi} alt="Coach Lexi" />
         </div>
         <div className="coaches__bio">
           <h3>Coach Lexi</h3>
-          <p>Lexi Grose is a former gymnast who fell in love with CrossFit in 2017. After receiving her Level 1 certification, Lexi brought her passion for community and newly-achieved coaching skills back to Strive.  She creates a training environment that is fun, focused, and challenging for everyone.
+          <p>
+            Lexi Grose is a former gymnast who fell in love with CrossFit in
+            2017. After receiving her Level 1 certification, Lexi brought her
+            passion for community and newly-achieved coaching skills back to
+            Strive. She creates a training environment that is fun, focused, and
+            challenging for everyone.
           </p>
-        </div>        
-
+          <p>Certifications: CF L1</p>
+        </div>
+      </div>
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
+        <div className="coaches__photo">
+          <Img fluid={props.jessica} alt="Coach Jessica" />
+        </div>
+        <div className="coaches__bio">
+          <h3>Coach Jessica</h3>
+          <p></p>
+          <p>
+            Certifications: CF L1, CF Mobility Certificate, CF Gymnastics
+            Certificate, Coached volleyball, basketball and track for 6 years
+          </p>
+        </div>
+      </div>
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
+        <div className="coaches__photo">
+          <Img fluid={props.rebekah} alt="Coach Rebekah" />
+        </div>
+        <div className="coaches__bio">
+          <h3>Coach Rebekah</h3>
+          <p>
+            The most important training I have ever had though is being a crisis
+            counselor for victims of domestic violence for 15 years. That is
+            where I learned to meet a person's emotional needs and that now has
+            brought me to a place at Strive where I can translate those tools
+            into CrossFit and coaching.
+          </p>
+          <p>Certifications: CF L1</p>
+        </div>
+      </div>
+      <div
+        data-sal="fade"
+        data-sal-duration="500"
+        data-sal-easing="ease-in-out"
+        className="coaches__row"
+      >
+        <div className="coaches__photo">
+          <Img fluid={props.sarah} alt="Coach Sarah" />
+        </div>
+        <div className="coaches__bio">
+          <h3>Coach Sarah</h3>
+          <p>
+            Sarah fell in love with fitness in 2014 after taking a weightlifting
+            class in college. It eventually led her to Crossfit Strive, where
+            she found home. She is passionate about evidence-based fitness,
+            strength training, and having fun while training hard.
+          </p>
+          <p>
+            Certifications: Bachelor's in Exercise and Sport Science with a
+            concentration in Health and Fitness Management from Texas State
+            (2020). NASM-CPT, CF L1
+          </p>
+        </div>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,38 +9,37 @@ import Pricing from "../components/pricing"
 import Schedule from "../components/schedule"
 import SEO from "../components/seo"
 import Coaches from "../components/coaches"
-import { FaEnvelope, FaFacebookSquare, FaInstagram } from 'react-icons/fa'
-import animateScrollTo from 'animated-scroll-to'
+import { FaEnvelope, FaFacebookSquare, FaInstagram } from "react-icons/fa"
+import animateScrollTo from "animated-scroll-to"
 import "../styles/main.scss"
 import Reviews from "../components/reviews"
 
 const IndexPage = () => {
-
   const [form, showForm] = useState(false)
 
   const scrollToForm = () => {
-    animateScrollTo(document.querySelector('.joinUs'))
+    animateScrollTo(document.querySelector(".joinUs"))
     showForm(true)
   }
 
   const scrollToClasses = () => {
-    animateScrollTo(document.querySelector('.classes'))
+    animateScrollTo(document.querySelector(".classes"))
   }
 
   const [navBackground, setNavBackground] = useState(false)
   const navRef = useRef()
-  
+
   navRef.current = navBackground
   useEffect(() => {
     const handleScroll = () => {
       const show = window.scrollY > 50
-      if (navRef.current !== show){
+      if (navRef.current !== show) {
         setNavBackground(show)
       }
     }
-    document.addEventListener('scroll', handleScroll)
+    document.addEventListener("scroll", handleScroll)
     return () => {
-      document.removeEventListener('scroll', handleScroll)
+      document.removeEventListener("scroll", handleScroll)
     }
   }, [])
 
@@ -123,14 +122,35 @@ const IndexPage = () => {
           }
         }
       }
-      kady: file(relativePath: { eq: "coach_kady.jpg" }) {
+      kady: file(relativePath: { eq: "Kady_headshot.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
           }
         }
       }
-      lexi: file(relativePath: { eq: "coach_lexi.jpg" }) {
+      lexi: file(relativePath: { eq: "Lexi_headshot.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      jessica: file(relativePath: { eq: "Jessica_headshot.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      rebekah: file(relativePath: { eq: "Rebekah_Headshot.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      sarah: file(relativePath: { eq: "sarah_Headshot.jpg" }) {
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
@@ -163,90 +183,182 @@ const IndexPage = () => {
 
   return (
     <div>
-      <Header 
-        onClick={scrollToForm}
-      />
+      <Header onClick={scrollToForm} />
       <SEO title="Home" />
       <div className="container">
-        <LeadContent 
-          onClick={scrollToForm}
-        />
-        <JoinUs 
+        <LeadContent onClick={scrollToForm} />
+        <JoinUs
           fluid={data.squat.childImageSharp.fluid}
           form={form}
           showForm={() => showForm(true)}
         />
-        <Schedule 
-          scroll={scrollToClasses}
-        />
+        <Schedule scroll={scrollToClasses} />
         <Pricing />
-        <Classes 
+        <Classes
           onClick={scrollToForm}
           fluid={data.kbswings.childImageSharp.fluid}
         />
-        <Coaches 
+        <Coaches
           athan={data.athan.childImageSharp.fluid}
           cody={data.cody.childImageSharp.fluid}
           kady={data.kady.childImageSharp.fluid}
           lexi={data.lexi.childImageSharp.fluid}
+          sarah={data.sarah.childImageSharp.fluid}
+          jessica={data.jessica.childImageSharp.fluid}
+          rebekah={data.rebekah.childImageSharp.fluid}
         />
-        <Reviews 
+        <Reviews
           best2018={data.best2018.childImageSharp.fluid}
           best2019={data.best2019.childImageSharp.fluid}
         />
         <div className="bottomPhotos left">
-          <div data-sal="slide-right" data-sal-duration="1000" data-sal-easing="ease-in-out"><Img fluid={data.pullups.childImageSharp.fluid} alt="Athlete performing pullups"/></div>
-          <div data-sal="slide-right" data-sal-duration="1000" data-sal-easing="ease"><Img fluid={data.postworkout.childImageSharp.fluid} alt="Coach giving athlete a fist bump after workout"/></div>  
+          <div
+            data-sal="slide-right"
+            data-sal-duration="1000"
+            data-sal-easing="ease-in-out"
+          >
+            <Img
+              fluid={data.pullups.childImageSharp.fluid}
+              alt="Athlete performing pullups"
+            />
+          </div>
+          <div
+            data-sal="slide-right"
+            data-sal-duration="1000"
+            data-sal-easing="ease"
+          >
+            <Img
+              fluid={data.postworkout.childImageSharp.fluid}
+              alt="Coach giving athlete a fist bump after workout"
+            />
+          </div>
         </div>
         <div className="bottomPhotos middle">
-          <Img className="middleImage" fluid={data.fullGym.childImageSharp.fluid} alt="CrossFit gym setting up full of athletes"/>
-          <div data-sal="fade" data-sal-duration="1000" data-sal-easing="ease-in-out" className="values">
+          <Img
+            className="middleImage"
+            fluid={data.fullGym.childImageSharp.fluid}
+            alt="CrossFit gym setting up full of athletes"
+          />
+          <div
+            data-sal="fade"
+            data-sal-duration="1000"
+            data-sal-easing="ease-in-out"
+            className="values"
+          >
             <h3>OUR VALUES</h3>
-            <p>FAIL OFTEN</p>  
-            <p>DO THE WORK</p>  
-            <p>OWN YOUR LIFE</p>  
-            <p>PRIORITIZE WHAT MATTERS</p>  
-            <p>KINDNESS OVER EVERYTHING</p>  
+            <p>FAIL OFTEN</p>
+            <p>DO THE WORK</p>
+            <p>OWN YOUR LIFE</p>
+            <p>PRIORITIZE WHAT MATTERS</p>
+            <p>KINDNESS OVER EVERYTHING</p>
           </div>
         </div>
         <div className="bottomPhotos right">
-          <div data-sal="slide-left" data-sal-duration="1000" data-sal-easing="ease"><Img fluid={data.boxjumps.childImageSharp.fluid} alt="Athlete jumping onto plyo box"/></div>
-          <div data-sal="slide-left" data-sal-duration="1000" data-sal-easing="ease-in-out"><Img fluid={data.humble.childImageSharp.fluid} alt="Team Hubmle Hustle posing for a picture"/></div>  
+          <div
+            data-sal="slide-left"
+            data-sal-duration="1000"
+            data-sal-easing="ease"
+          >
+            <Img
+              fluid={data.boxjumps.childImageSharp.fluid}
+              alt="Athlete jumping onto plyo box"
+            />
+          </div>
+          <div
+            data-sal="slide-left"
+            data-sal-duration="1000"
+            data-sal-easing="ease-in-out"
+          >
+            <Img
+              fluid={data.humble.childImageSharp.fluid}
+              alt="Team Hubmle Hustle posing for a picture"
+            />
+          </div>
         </div>
-        <form name='strive-contact' data-netlify='true' netlify-honeypot='bot-field' hidden>
-          <input type='text' name='name' />
-          <input type='email' name='email' />
-          <input type='tel' name='phone' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
-          <input type='button' name='reason' />
-          <input type='button' name='reason' />
-          <input type='text' name='otherDescription' />
-          <textarea name='comments' />
+        <form
+          name="strive-contact"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          hidden
+        >
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
+          <input type="button" name="reason" />
+          <input type="button" name="reason" />
+          <input type="text" name="otherDescription" />
+          <textarea name="comments" />
         </form>
-        <form name='strive-hold' data-netlify='true' netlify-honeypot='bot-field' hidden>
-          <input type='text' name='name' />
-          <input type='email' name='email' />
-          <input type='text' name='date' />
-          <input type='textarea' name='reason' />
+        <form
+          name="strive-hold"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          hidden
+        >
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <input type="text" name="date" />
+          <input type="textarea" name="reason" />
         </form>
-        <form name='strive-cancel' data-netlify='true' netlify-honeypot='bot-field' hidden>
-          <input type='text' name='name' />
-          <input type='email' name='email' />
-          <textarea name='comments' />
+        <form
+          name="strive-cancel"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          hidden
+        >
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <textarea name="comments" />
         </form>
-
       </div>
       <footer className="footer">
         <div className="footer__links">
-          <a href="https://facebook.com/crossfitstrivebastrop" target="_blank" rel="noreferrer noopener"><FaFacebookSquare /></a>
-          <a href="https://instagram.com/crossfitstrivebastrop" target="_blank" rel="noreferrer noopener"><FaInstagram /></a>
-          <a href="mailto:info@crossfitstrive.com" target="_blank" rel="noreferrer noopener"><FaEnvelope /></a>
+          <a
+            href="https://facebook.com/crossfitstrivebastrop"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaFacebookSquare />
+          </a>
+          <a
+            href="https://instagram.com/crossfitstrivebastrop"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="mailto:info@crossfitstrive.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaEnvelope />
+          </a>
         </div>
         <div className="footer__contact">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13805.434488145698!2d-97.346715!3d30.11255!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x770de78066f1da5e!2sCrossFit%20Strive%20Bastrop!5e0!3m2!1sen!2sus!4v1575587420411!5m2!1sen!2sus" maxwidth="600" maxheight="450" frameBorder="0" style={{border:0}} allowFullScreen=""></iframe>  
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13805.434488145698!2d-97.346715!3d30.11255!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x770de78066f1da5e!2sCrossFit%20Strive%20Bastrop!5e0!3m2!1sen!2sus!4v1575587420411!5m2!1sen!2sus"
+            maxwidth="600"
+            maxheight="450"
+            frameBorder="0"
+            style={{ border: 0 }}
+            allowFullScreen=""
+          ></iframe>
           <a href="tel:+1-512-555-9876">512-555-9876</a>
-          <a href="https://goo.gl/maps/B362Ye6MUBcPYLvy8" target="_blank" rel="noreferrer noopener"><p>623 Highway 71 West, Suite 108</p><p>Bastrop, TX 78602</p></a>
-          <Link className="membership" to="/membershiphold">Membership Hold</Link>
-          <Link className="membership" to="/membershipcancellation">Membership Cancellation</Link>
+          <a
+            href="https://goo.gl/maps/B362Ye6MUBcPYLvy8"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <p>623 Highway 71 West, Suite 108</p>
+            <p>Bastrop, TX 78602</p>
+          </a>
+          <Link className="membership" to="/membershiphold">
+            Membership Hold
+          </Link>
+          <Link className="membership" to="/membershipcancellation">
+            Membership Cancellation
+          </Link>
         </div>
         © {new Date().getFullYear()} CrossFit Strive Bastrop
       </footer>
